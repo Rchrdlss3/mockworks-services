eb10b7723cb946249ff2e25c5c6418d2f997410f
Richard Ulysse <Rchrdlss3@gmail.com>
2026-04-02 13:49:17 -0400

    removed app using router twice, causing CORS error

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,20 @@ import { router } from "./routes/index";
 import { connectToDatabase } from "./utils/database";
 import type { Response, Request } from "express";
 
+const app = express();
+
 const corsOptions = {
   origin: ["http://localhost:3000","https://rchrdlss3.github.io", "https://rchrdlss3.github.io/mockworks"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+  credentials: true
 };
+app.use(cors(corsOptions));
 
-  const app = express();
 
-  
+  app.use(express.json());
   app.get('/',(req:Request, res:Response) => {
     res.status(200).json('Welcome to Mockworks Services')
   });
-  
-  app.use(router);
-
-  app.use(cors(corsOptions));
-  app.use(express.json());
   app.use(router);
   app.listen(8000)
   connectToDatabase();
