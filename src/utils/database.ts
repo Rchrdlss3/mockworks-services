44af879f2144e5b74956392fb0d8525166655f97
Richard Ulysse <Rchrdlss3@gmail.com>
2026-04-02 13:45:26 -0400

    added vercel helper function for cached connection to assist with connection error

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
+import { attachDatabasePool } from "@vercel/functions";
+
+let cachedConnection:  typeof mongoose | null = null;
 
 export async function connectToDatabase(): Promise<void> {
+  if (cachedConnection) {
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.DATABASE_URL as string);
+    const connection  = await mongoose.connect(process.env.DATABASE_URL as string);
+    attachDatabasePool(connection.connection.getClient());
+    cachedConnection = connection
   } catch (e) {
+    cachedConnection = null;
     console.error(e);
   }
 }
